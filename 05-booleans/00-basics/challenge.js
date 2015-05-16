bef0758dbd3862b7f6f334b916c4337baade3be5
Brookelyn <brooke.balza@gmail.com>
2015-05-16 11:43:55 +0000

    Boolean tests now working

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,19 +1,23 @@
 module.exports.equalStrings = function(stringOne, stringTwo) {
-
+	return (stringOne == stringTwo);
 };
 
 module.exports.notEqual = function(one, two) {
-
+	return (one !== two);
 };
 
 module.exports.inBetween = function(lower, middle, upper) {
-
+	return ((lower < middle) && (middle < upper));
 };
 
 module.exports.outsideRanges = function(number) {
+	return (((number < 2) || (number > 5)) && (number < 10 || 20 < number) && (number <= 42 || 75 < number));
+};
 
+module.exports.outsideRanges = function(number) {	
+	return (((number < 2) || (number > 5)) && (number < 10 || 20 < number) && (number <= 42 || 75 < number));
 };
 
 module.exports.nameAndPrice = function(name, price) {
-
+	return ( (name === "NYTimes" || name === "LATimes") && (price >= 1));
 };
