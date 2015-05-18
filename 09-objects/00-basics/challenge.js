f0102c67b16c896ea6bc6c625d1b9dbe47884906
Brookelyn <brooke.balza@gmail.com>
2015-05-18 15:09:35 +0000

    Object exercises tests now passing

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,33 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents){
+	var course = {
+		title: courseTitle,
+		duration: courseDuration,
+		students: courseStudents
+	};
+	return course;
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object, newProp, newValue){
+	if (object.hasOwnProperty(newProp)) {
+		return object;
+	}
+	else{
+		object[newProp] = newValue;
+		return object;
+	}
+};
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(letter){
+	return ("Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\n" + "Sincerely,\n" + letter.sender);
+};
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item, money){
+	var apple = {
+		'MacBook Air': 999,
+		'MacBook Pro': 1299,
+		'Mac Pro': 2499,
+		'Apple Sticker': 1
+	};
+		
+	return apple[item] <= money;	
+};
